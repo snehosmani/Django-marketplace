901e1c2ea62fb8a208fb45d91de59662dbb819c1
Snehal Hosmani <114698945+snehosmani@users.noreply.github.com>
2023-02-03 22:36:16 -0500

    Update settings.py

--- puddle/puddle/settings.py
+++ puddle/puddle/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3sxtb6^0#$9c(ro^sss3oa%nsoa=2iazqb!l_9c4&f=zaiq!%d'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
